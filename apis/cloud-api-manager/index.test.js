d6045b2275072589a1f6664d1800c29e6622f5dc
Nsikan Essien <20339399+noce2@users.noreply.github.com>
2020-06-25 19:12:21 +0100

    Add placeholder for index tests

---
+++ apis/cloud-api-manager/index.test.js
@@ -0,0 +1,11 @@
+// jshint esversion:6
+const { describe, it } = require("mocha");
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+describe("Index", function () {
+    it("should run successfully and return exit code of 0")
+})
